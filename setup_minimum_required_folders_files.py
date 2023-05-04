7833d4fe592c417cf426ad1135760ec5e2b627cb
James Mason <jmason86@gmail.com>
2023-05-04 10:48:43 -0400

    Download synthetic fits file

--- setup_minimum_required_folders_files.py
+++ setup_minimum_required_folders_files.py
@@ -5,14 +5,13 @@ from urllib.parse import urlparse
 import ssl
 
 
-tmp_file_urls = [] # dl=1 is important for dropbox links
+tmp_file_urls = ['https://www.dropbox.com/s/mnzrchcffctxpkq/config_default.fits?dl=1'] # dl=1 is important for dropbox links
 
 def run():
     if os.getenv('suncet_data') == None:
         os.environ['suncet_data'] = './'
-    
-    example_path = Path('./ancillary/')
-    example_path.mkdir(parents=True, exist_ok=True)
+    synthetic_path = Path('./synthetic/level0_raw/fits/')
+    synthetic_path.mkdir(parents=True, exist_ok=True)
 
     ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -21,8 +20,8 @@ def run():
         data = thisurl.read()
         thisurl.close()
         filename = get_filename_from_url(url)
-        if filename.endswith('fullemiss.nc'):
-            filename = example_path / filename
+        if filename.endswith('.fits'):
+            filename = synthetic_path / filename
         with open(filename, "wb") as f:
             print('downloading file: {}'.format(filename))
             f.write(data)
