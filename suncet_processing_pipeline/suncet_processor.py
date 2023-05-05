f5e659127c76d94422bb335b4356f93068c8897e
James Mason <jmason86@gmail.com>
2023-05-05 14:00:29 -0400

    bare bones wrapper script

---
+++ suncet_processing_pipeline/suncet_processor.py
@@ -0,0 +1,25 @@
+"""
+This is the main wrapper for most/all(?) of the other processor related python files
+"""
+import os
+from glob import glob
+import astropy.units as u
+import sunpy.map
+from suncet_processing_pipeline import config_parser
+
+class Processor:
+    def __init__(self, config_filename=os.getcwd() + '/suncet_processing_pipeline/config_files/config_default.ini'):
+        self.config_filename = config_filename
+        self.config = self.__read_config(config_filename)
+
+    def __read_config(self, config_filename):   
+        return config_parser.Config(config_filename)
+
+
+    def run(self):
+        pass
+    
+
+if __name__ == "__main__":
+    processor = Processor()
+    processor.run()
