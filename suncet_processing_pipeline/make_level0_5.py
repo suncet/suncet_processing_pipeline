00be6bc0b81e90a12a39f636662992c77cc94581
James Mason <jmason86@gmail.com>
2024-05-30 16:43:45 -0400

    Convert an example CTIM beacon packet into engineering units. SunCET packets will be _mostly_ the same definition

--- suncet_processing_pipeline/make_level0_5.py
+++ suncet_processing_pipeline/make_level0_5.py
@@ -1,12 +1,14 @@
 """
 This is the code to make the Level 0.5 data product. 
-We use 0_5 in the filename to prevent issues with some operating systems
+We use 0_5 in the filename to prevent issues with some operating systems not being able to handle a period in a filename
 """
 import os
 from glob import glob
 import astropy.units as u
 import sunpy.map
 from suncet_processing_pipeline import config_parser
+from suncet_processing_pipeline.packet_definitions import beacon_definition
+
 
 class Level0_5:
     def __init__(self, config_filename=os.getcwd() + '/suncet_processing_pipeline/config_files/config_default.ini'):
@@ -23,8 +25,15 @@ class Level0_5:
 
 
     def run(self):
+        filename = os.getenv('suncet_data') + '/ctim_data/data_8646760_2023-12-03T23-32-22'
+        with open(filename, 'rb') as file:
+            binary_data = file.read()
+
+        parsed_beacon = beacon_definition.packet.parse(binary_data)
+
+        print(parsed_beacon)
         pass
-    
+
 
 if __name__ == "__main__":
     level0_5 = Level0_5()
