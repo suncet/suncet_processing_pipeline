8a08564b4860831740c0ac0a0b4e50aa66ad0cb4
Daniel da Silva <daniel.e.dasilva@nasa.gov>
2025-03-31 15:04:43 -0400

    FitsMetadataManager class runs end to end

--- suncet_processing_pipeline/metadata_mgr.py
+++ suncet_processing_pipeline/metadata_mgr.py
@@ -1,9 +1,19 @@
+"""
+
+"""
 import pandas as pd
 from pathlib import Path
 
 
-class MetadataManager:
-    """Class for interacting with SunCET Metadata files.
+# Expected name of FITS metadata definitions file in the run directory
+FITS_METADATA_DEFINITIONS_FILENAME = 'suncet_metadata_definition_fits.csv'
+
+# Expected name of the meta version text file in the run directory
+FITS_METADATA_VERSION_FILENAME = 'suncet_metadata_definition_version.txt'
+
+
+class FitsMetadataManager:
+    """Class for interacting with SunCET FITS Metadata files.
 
     This expect the metadata is downloaded into the run directory. To do that,
     see: setup_minimum_required_folders_files.py
@@ -15,9 +25,12 @@ class MetadataManager:
         Args
            run_dir: Path to run directory
         """
+        # Convert run directory to a Path object
+        run_dir = Path(run_dir)
+        
         # Set paths and check they exist
-        self._metadata_path = Path(run_dir) / 'suncet_metadata_definition.csv'
-        self._metadata_ver_path = Path(run_dir) / 'suncet_metadata_definition_version.csv'
+        self._metadata_path = run_dir / FITS_METADATA_DEFINITIONS_FILENAME
+        self._metadata_ver_path = run_dir / FITS_METADATA_VERSION_FILENAME
 
         if not self._metadata_path.exists():
             raise FileNotFoundError(
@@ -31,8 +44,15 @@ class MetadataManager:
 
         # Load metadata CSV using Pandas
         print(f'Reading metadata from {self._metadata_path}')
-        self._metadata_df = pd.read_csv(self._metadata_path)
-        self._metadata_df = _clean_metadata_comments(self._metadata_df)
+        self._df_metadata = pd.read_csv(self._metadata_path)
+
+        # Group internal names by the comment block they are in. This
+        # is a dictionary that maps group name to a list of internal
+        # names
+        self._metadata_groups = _get_metadata_groups(self._df_metadata)
+
+        # Remove comments from datafarme after this
+        self._df_metadata = _clean_metadata_comments(self._df_metadata)
         
         # Load metadata version (just read string from text file)
         with open(self._metadata_ver_path) as fh:
@@ -41,89 +61,62 @@ class MetadataManager:
         print(f'Found metadata version "{self._metadata_ver}"')
 
         # Convert metadata df to dictionary mapping internal name to dictionary
-        # of columns -> values
-        self._metadata_dict = _get_metadata_dict(self._metadata_df)
+        # of spreadsheet's columns to cell contents
+        self._metadata_dict = _get_metadata_dict(self._df_metadata)
 
-    def get_netcdf4_variable_name(self, internal_name):
-        """Get name of variable for writing to a NetCDF4 file
+        
+        # Define variable that will be used to store values. Maps internal name
+        # to values
+        self._metadata_values = {}        
+        
+    def load_from_dict(self, metadata_values):
+        """Load metadata values from a dictionary. 
+
+        This can be called subsequent times to incrementally add metadata
+        values.
 
         Args
-          internal_name: Internal name of variable (within code)
-        Returns
-          what that internal name should be called in a NetCDF4 file
-        """
-        # Ensure variable is in the metadata dictionary
-        if internal_name not in self._metadata_dict:
-            raise RuntimeError(
-                f"Could not find metadata for variable with "
-                f"internal name '{internal_name}'"
-            )
+           metadata_values: keys should be metadata internal names
+        """        
+        self._metadata_values.update(metadata_values)
 
-        # Get the variable name, raising Exception if its not filled out in the
-        # table
-        var_name = self._metadata_dict[internal_name]['netCDF variable name']
-
-        if not var_name:
-            raise RuntimeError(
-                f"Needed NetCDF variable name for internal name "
-                f"{internal_name}\", but missing"
-            )
-
-        # Return good result
-        return var_name
-
-    def get_netcdf4_dimension_names(self, internal_name):
-        """Get tuple of dimension names for the given variable.
+    def generate_fits_header(self, fits_file):        
+        # Build list of things to write, organized by the groups they are
+        # in with care to preserve their order
+        vars_with_values = set(self._metadata_values.keys())
+        counter = 0
+        to_write = []
         
-        Args
-          internal_name: Internal name of variable (within code)
-        Returns
-          tuple of dimension names
-        """
-        # Ensure variable is in the metadata dictionary
-        if internal_name not in self._metadata_dict:
-            raise RuntimeError(
-                f"Could not find metadata for variable with internal name "
-                f"'{internal_name}'."
-            )
-        # Load variable dict and return subset of keys that are relevant        
-        var_dict = self._metadata_dict[internal_name]
-        dim_csv = var_dict['netCDF dimensions']
+        for group_name, group_variables in self._metadata_groups.items():
+            # Do nothing if no variables with values in this group
+            if len(set(group_variables) & vars_with_values) == 0:
+                continue
+
+            to_write.append((counter, 'COMMENT', group_name.center(72, '-')))
+            counter += 1
+            
+            for group_var in group_variables:
+                if group_var in vars_with_values:
+                    name = self._metadata_dict[group_var]['FITS variable name']
+                    description = self._metadata_dict[group_var]['Description']
+
+                    value = self._metadata_values[group_var]
+                    to_write.append((counter, name, value, description))
+                    counter += 1
+
+        # Write to_write items to fits header
+        header = fits_file[0].header
         
-        if dim_csv:        
-            return tuple(dim_csv.split(','))
-        else:
-            return tuple()  # empty tuple
-    
-    
-    def get_netcdf4_attrs(self, internal_name):
-        """Get dictionary of static NetCDF4 attributes for a given variable.
-
-        Args
-          internal_name: Internal name of variable (within code)
-        Returns
-          dictionary of attribute keys to values
-        """
-        # Ensure variable is in the metadata dictionary
-        if internal_name not in self._metadata_dict:
-            raise RuntimeError(
-                f"Could not find metadata for variable with internal name "
-                f"'{internal_name}'."
-            )
-        # Load variable dict and return subset of keys that are relevant        
-        var_dict = self._metadata_dict[internal_name]
-
-        return {
-            "units": var_dict["units (human)"]    
-        }
+        for index, *args in to_write:
+            header.insert(index, tuple(args))
         
         
-def _get_metadata_dict(metadata_df):
+def _get_metadata_dict(df_metadata):
     """Convert metadata dataframe to dictinoary mapping internal name
     to dictionary of cols to values.
 
     Args
-      metadata_df: Metadata dictionary as loaded from flie with comments
+      df_metadata: Metadata dictionary as loaded from flie with comments
         cleaned
     Returns
        dictionary mapping internal names to dictionaries holding the
@@ -131,16 +124,15 @@ def _get_metadata_dict(metadata_df):
     """
     metadata_dict = {}
     
-    for _, row in metadata_df.iterrows():
-        cur_dict = {col: row[col] for col in metadata_df.columns}
+    for _, row in df_metadata.iterrows():
+        cur_dict = {col: row[col] for col in df_metadata.columns}
         cur_key = row['Internal Variable Name']
-
         metadata_dict[cur_key] = cur_dict
 
     return metadata_dict
         
 
-def _clean_metadata_comments(metadata_df):
+def _clean_metadata_comments(df_metadata):
     """Remove comment rows from the metadata Data Frame.
     
     A command has the work "COMMENT" in the first column
@@ -151,11 +143,39 @@ def _clean_metadata_comments(metadata_df):
       dataframe with comment row dropped
     """
     collected_rows = []
-    first_row = metadata_df.columns[0]
+    first_row = df_metadata.columns[0]
     
-    for _, row in metadata_df.iterrows():
-        if 'COMMENT' not in row[first_row].upper():
+    for _, row in df_metadata.iterrows():
+        if 'COMMENT' not in str(row[first_row]).upper():
             collected_rows.append(row)
         
     return pd.DataFrame(collected_rows)
         
+
+def _get_metadata_groups(df_metadata):
+    """Get dictionary that maps group names (COMMENT blocks) to
+    list of internal variable names in that group.
+    
+    Args
+      dataframe as loaded directly from CSV file
+    Returns
+       dictionary mapping group names to list of internal
+       variable names
+    """
+    row_groups = {}
+    group_name = None
+    first_row = df_metadata.columns[0]
+
+    for _, row in df_metadata.iterrows():
+        if 'COMMENT' in str(row[first_row]).upper():
+            group_name = (
+                row[first_row]
+                .replace('COMMENT', '')
+                .replace('-', '')
+                .strip()
+            )
+            row_groups[group_name] = []            
+        elif group_name:
+            row_groups[group_name].append(row['Internal Variable Name'])
+
+    return row_groups
